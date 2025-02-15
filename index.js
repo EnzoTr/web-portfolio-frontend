41b7dac5b4dba50f6f602f541a987a4eafbbbfec
EnzoTr <enzotur05@hotmail.com>
2025-02-14 21:26:34 -0300

    added mobile responsive

--- index.js
+++ index.js
@@ -27,3 +27,58 @@ const swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev"
     }
 });
+
+(function(){
+    const listElements = document.querySelectorAll('.nav-item--show');
+    const list = document.querySelector('.links-container');
+    const menu = document.querySelector('.ham');
+
+    const addClick = ()=>{
+        listElements.forEach(element =>{
+            element.addEventListener('click',()=>{
+
+                let subMenu = element.children[1]
+                let height = 0;
+                element.classList.toggle('menu-item--active');
+
+                if(subMenu.clientHeight === 0){
+                    height = subMenu.scrollHeight;
+                }
+
+                subMenu.style.height = `${height}px`;
+
+            });
+        });
+    }
+
+
+    const deleteStyleHeight = ()=>{
+        listElements.forEach(element =>{
+            if(element.children[1].getAttribute ('style')){
+                element.children[1].removeAttribute ('style');
+                element.classList.remove('menu-item--active');
+            }
+        })
+    }
+
+    window.addEventListener('resize',()=>{
+        if(window.innerWidth > 1200) {
+            deleteStyleHeight();
+            if (list.classList.contains('links-container--show')) {
+                list.classList.remove('links-container--show');
+            }
+    
+        }else{
+            addClick();
+        }
+    });
+
+    if(window.innerWidth <= 1200) {
+        addClick();
+    }
+
+    menu.addEventListener('click',()=>{
+        list.classList.toggle('links-container--show')
+    })
+
+})();  
